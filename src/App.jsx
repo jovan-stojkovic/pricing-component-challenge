5293d6e870bab18bd0d2202139920ffc60d8d60f
jovan-stojkovic <code.jovanstojkovic@gmail.com>
2024-04-06 01:10:41 +0200

    desktop version is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,94 @@
+import { useState } from "react";
 import "./Styles.scss";
 
 const App = () => {
+  const [basicPrice, setBasicPrice] = useState(199.99);
+  const [proPrice, setProPrice] = useState(249.99);
+  const [masterPrice, setMasterPrice] = useState(399.99);
+
+  const handleCheck = () => {
+    setBasicPrice(basicPrice === 19.99 ? 199.99 : 19.99);
+    setProPrice(proPrice === 24.99 ? 249.99 : 24.99);
+    setMasterPrice(masterPrice === 39.99 ? 399.99 : 39.99);
+  };
+
   return (
     <main>
+      <div className="background-cont-bottom"></div>
+      <div className="background-cont-top"></div>
       <div className="app">
         <h1>Our Pricing</h1>
 
         <div className="toggle-cont">
           <p>Annually</p>
-          <input type="checkbox" />
+
+          <div className="checkbox-wrapper-7">
+            <input
+              className="tgl tgl-ios"
+              id="cb2-7"
+              type="checkbox"
+              onClick={handleCheck}
+            />
+            <label className="tgl-btn" htmlFor="cb2-7" />
+          </div>
+
           <p>Monthly</p>
         </div>
-      </div>
 
-      <div className="info-container">
-        <div className="side">
-          <h2>Basic</h2>
-          <p className="price">
-            <span>$</span>19.99
-          </p>
-          <p>500 GB Storage</p>
-          <p>2 Users Allowed</p>
-          <p>Send up to 3 GB</p>
-          <button className="side">LEARN MORE</button>
-        </div>
-        <div className="center">
-          <h2>Professional</h2>
-          <p className="price">
-            <span>$</span>24.99
-          </p>
-          <p>1 TB Storage</p>
-          <p>5 Users Allowed</p>
-          <p>Send up to 10 GB</p>
-          <button className="center">LEARN MORE</button>
-        </div>
-        <div className="side">
-          <h2>Master</h2>
-          <p className="price">
-            <span>$</span>39.99
-          </p>
-          <p>2 TB Storage</p>
-          <p>10 Users Allowed</p>
-          <p>Send up to 20 GB</p>
-          <button className="side">LEARN MORE</button>
+        <div className="info-container">
+          <div className="side">
+            <h2>Basic</h2>
+            <h3 className="price">
+              <span>$</span>
+              {basicPrice}
+            </h3>
+            <div className="p-cont">
+              <p>500 GB Storage</p>
+            </div>
+            <div className="p-cont">
+              <p>2 Users Allowed</p>
+            </div>
+            <div className="p-cont last">
+              <p>Send up to 3 GB</p>
+            </div>
+            <button className="side-btn">LEARN MORE</button>
+          </div>
+
+          <div className="side center">
+            <h2>Professional</h2>
+            <h3 className="price">
+              <span>$</span>
+              {proPrice}
+            </h3>
+            <div className="p-cont">
+              <p>1 TB Storage</p>
+            </div>
+            <div className="p-cont">
+              <p>5 Users Allowed</p>
+            </div>
+            <div className="p-cont lst">
+              <p>Send up to 10 GB</p>
+            </div>
+            <button className="center-btn">LEARN MORE</button>
+          </div>
+
+          <div className="side">
+            <h2>Master</h2>
+            <h3 className="price">
+              <span>$</span>
+              {masterPrice}
+            </h3>
+            <div className="p-cont">
+              <p>2 TB Storage</p>
+            </div>
+            <div className="p-cont">
+              <p>10 Users Allowed</p>
+            </div>
+            <div className="p-cont last">
+              <p>Send up to 20 GB</p>
+            </div>
+            <button className="side-btn">LEARN MORE</button>
+          </div>
         </div>
       </div>
     </main>
